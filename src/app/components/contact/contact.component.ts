7774b5c71eb3f3f55bff9dcb67cfd0cdc08c241f
Juan Quiroz <juan.quiroz1009@gmail.com>
2022-11-17 16:36:05 -0500

    jquery_bxslider

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,21 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-import * as $ from 'jquery'
 
+declare const $:any;
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+  styleUrls: ['./contact.component.css'],
 })
 export class ContactComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
-    $("#logo").click(function(e){
+    $('#logo').click(function (e: any) {
       e.preventDefault();
-      $("header").css("background", "green")
-                 .css("height", "50px")
-    })
+      $('header').css('background', 'green').css('height', '90px');
+    });
+    $('.galeria').bxSlider({
+      mode: 'fade',
+      captions: true,
+      slideWidth: 400,
+    });
   }
-
 }
